80e6115af70c7cf855a1625faec56c07febc54f1
Qixxxxx <1143541029@qq.com>
2022-04-13 19:06:24 +0800

    添加文件

---
+++ my_git/src/main/java/com/qx/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.qx.git;
+
+/**
+ * @author QiXin
+ * @version v0.0
+ * @Time 2022-04-13-17:40
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
