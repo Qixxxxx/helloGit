8c4c067aa6d8e9cdfbf71d691057690ea21b75cc
Qixxxxx <1143541029@qq.com>
2022-04-22 22:22:30 +0800

    创建dev分支

---
+++ my_git/src/main/java/com/qx/git/DevBranch.java
@@ -0,0 +1,12 @@
+package com.qx.git;
+
+/**
+ * @author QiXin
+ * @version v0.0
+ * @Time 2022-04-22-22:21
+ */
+public class DevBranch {
+    public static void main(String[] args) {
+        System.out.println("创建dev分支");
+    }
+}
